a62ca95900a53cd1e16ecb25f8f97b25ecfa4fe0
Altay Simsek <altaysimsek16@gmail.com>
2023-08-12 20:24:12 +0300

    feat: fetch datas client side

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,9 +1,10 @@
 
+import { NextResponse } from 'next/server'
 import fsPromises from 'fs/promises';
 import path from 'path'
-import { NextResponse } from '../../../node_modules/next/server';
 
 export async function GET(request: Request) {
+
     const filePath = path.join(process.cwd(), 'src/json/data.json');
     const jsonData = await fsPromises.readFile(filePath);
     const objectData = JSON.parse(jsonData.toString());
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
+'use client'
+import useSWR from 'swr'
 
-export default function Home() {
-  return (
-    <main>Arkhio</main>
-  )
+const fetcher = (...args: any) => fetch(...args).then(res => res.json())
+
+export default  function Home() {
+const { data, error, isLoading } = useSWR('/api/links', fetcher)
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  // render data
+  return <div>hello!
+    {data.posts.map((post: any) =>(<div key={post.id}>{post.title}</div>))}
+  </div>
+  
 }
